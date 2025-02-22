333ad874bd06653262e8586053b0871a738b2959
oscar0076 <y.chaari216@gmail.com>
2025-02-22 21:32:05 +0100

    flask1

--- app.py
+++ app.py
@@ -6,6 +6,11 @@ from tensorflow.keras.models import load_model
 from tensorflow.keras.losses import MeanSquaredError
 
 app = Flask(__name__)
+FIELD_CAPACITY = 0.35  # Maximum soil moisture (m³/m³)
+ROOT_DEPTH = 0.6  # Root zone depth (meters)
+SOIL_TYPE_FACTOR = 1000  # Converts m³/m³ to mm
+DAYS_NUM = 7  # Number of days to predict
+
 
 # Charger le modèle entraîné
 model = load_model('fine_tuned_model.h5', custom_objects={'mse': MeanSquaredError()})
@@ -47,8 +52,11 @@ def predict():
         
         # Dénormalisation de la prédiction
         predicted_value = prediction[0, 0] * (feature_max[-1] - feature_min[-1]) + feature_min[-1]
-
-        return jsonify({"SoilMoistureIndex": float(predicted_value)})
+        water_needed = (FIELD_CAPACITY - predicted_value.flatten()) * ROOT_DEPTH * SOIL_TYPE_FACTOR * DAYS_NUM
+        water_needed = np.maximum(water_needed, 0)
+        
+        return jsonify({"SoilMoistureIndex": float(predicted_value),
+                        "Water Needed for Next 7 Days(mm):": water_needed[0]})
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
